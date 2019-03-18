8db5fc97f888e6a2e6354654ccec136a7e7b2285
junjiecjj <32957066+junjiecjj@users.noreply.github.com>
2019-03-19 00:34:44 +0800

    Create 题2—顺时针打印矩阵.cpp

---
+++ target_offer/题2—顺时针打印矩阵.cpp
@@ -0,0 +1,31 @@
+/*************************************************************************
+>> File Name: 顺时针打印矩阵.cpp
+>> Author: chenjunjie
+>> Mail: 2716705056qq.com
+>> Created Time: 2019.03.18
+************************************************************************/
+
+#include<iostream>
+using namespace std;
+#include<vector>
+
+
+class Solution {
+public:
+    vector<int> printMatrix(vector<vector<int> > matrix) {
+        int raw = matrix.size();                          //矩阵行数
+        int col = matrix[0].size();                       //矩阵列数
+        vector<int> result;
+        if(m == 0|| n == 0)
+            return result;
+        int lu = 0;
+        int ld = 0;
+        int ru = raw - 1;
+        int rd = col - 1;
+        while(lu <= ru && ld <= rd)
+        {
+            for(int i  = )
+        }
+
+    }
+};
