d6c69bdc54f9e5f2c546c4b37fad97af3bdbace5
junjiecjj <32957066+junjiecjj@users.noreply.github.com>
2019-03-22 23:52:36 +0800

    Update 题10.二叉树中和为某一值的所有路径.cpp

--- target_offer/题10.二叉树中和为某一值的所有路径.cpp
+++ target_offer/题10.二叉树中和为某一值的所有路径.cpp
@@ -7,19 +7,33 @@
 
 #include<iostream>
 using namespace std;
+#include<vector>
 
 struct TreeNode {
-	int val;
-	struct TreeNode *left;
-	struct TreeNode *right;
-	TreeNode(int x) :
-			val(x), left(NULL), right(NULL) {
-	}
-};
-
-class Solution {
-public:
-    vector<vector<int> > FindPath(TreeNode* root,int expectNumber) {
-
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+    TreeNode(int x) :
+            val(x), left(NULL), right(NULL) {
     }
 };
+class Solution {
+public:
+    vector<int> tmp;
+    vector<vector<int>> buffer;
+    vector<vector<int> > FindPath(TreeNode* root,int expectNumber) {
+        if(root==NULL) return buffer;
+        tmp.push_back(root->val);
+        if(expectNumber-root->val==0&&root->left==NULL&&root->right==NULL){
+            buffer.push_back(tmp);
+        }
+        FindPath(root->left,expectNumber-root->val);
+        FindPath(root->right,expectNumber-root->val);
+        if(tmp.size()!=0)
+            tmp.pop_back();
+        return buffer;
+ 
+    }
+};
+
+
