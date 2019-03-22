55370d2aafef7452ca2c3f3dc3e9cea8b4e1b911
junjiecjj <32957066+junjiecjj@users.noreply.github.com>
2019-03-22 15:18:10 +0800

    Update 题5:  二叉树的镜像.cpp

--- target_offer/题5:  二叉树的镜像.cpp
+++ target_offer/题5:  二叉树的镜像.cpp
@@ -39,7 +39,17 @@ struct TreeNode {
 };
 class Solution {
 public:
-    void Mirror(TreeNode *pRoot) {
-        
+    void Mirror(TreeNode *pRoot){
+        if(pRoot == NULL)
+            return;
+        else
+        {
+            TreeNode *tmp = pRoot -> left;
+            pRoot -> left = pRoot -> right;
+            pRoot -> right = tmp;
+            Mirror(pRoot -> left);
+            Mirror(pRoot -> right);
+            return;
+        }
     }
 };
