8ab4d1e205bfbe695850477c5d1e71c05bb868b7
junjiecjj <32957066+junjiecjj@users.noreply.github.com>
2019-03-22 16:37:49 +0800

    Create 题9.二叉树的后序遍历.cpp

---
+++ target_offer/题9.二叉树的后序遍历.cpp
@@ -0,0 +1,21 @@
+/*************************************************************************
+>> File Name: 二叉树的后序遍历.cpp
+>> Author: chenjunjie
+>> Mail: 2716705056qq.com
+>> Created Time: 2019.03.22
+
+
+题目描述
+输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历的结果。如果是则输出Yes,否则输出No。假设输入的数组的任意两个数字都互不相同。
+************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+
+class Solution {
+public:
+    bool VerifySquenceOfBST(vector<int> sequence) {
+
+    }
+};
