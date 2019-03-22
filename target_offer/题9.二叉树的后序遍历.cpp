c6df749f0bcaffaa21bf41ade7a66bb9c65a279f
junjiecjj <32957066+junjiecjj@users.noreply.github.com>
2019-03-22 22:36:47 +0800

    Update 题9.二叉树的后序遍历.cpp

--- target_offer/题9.二叉树的后序遍历.cpp
+++ target_offer/题9.二叉树的后序遍历.cpp
@@ -11,11 +11,45 @@
 
 #include<iostream>
 using namespace std;
+#include<vector>
 
-
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};
 class Solution {
 public:
+    int flag = 0;//一定要设置这个变量，当第一次迭代时为0，防止最开始就是空序列，之后变为1，随后的子序列可以为空
     bool VerifySquenceOfBST(vector<int> sequence) {
-
+        int len = sequence.size();
+        if(len == 0 && flag == 0)
+            return false;
+        flag = 1;
+        if(len <= 2)
+            return true;
+        vector<int> left;
+        vector<int> right;
+        int mid = 0;
+        while(sequence[mid] < sequence[len-1])
+        {
+            left.push_back(sequence[mid]);
+            ++mid;
+        }
+        for(int i = mid; i < len-1; i++)
+        {
+            if(sequence[i] <= sequence[len-1])
+            {
+                return false;
+            }
+            else
+            {
+                right.push_back(sequence[i]);
+            }
+        }
+        return VerifySquenceOfBST(left) && VerifySquenceOfBST(right);
     }
 };
