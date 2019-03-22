fb911624986dcdf3fd3c44be6ef1d42ad92d59c8
junjiecjj <32957066+junjiecjj@users.noreply.github.com>
2019-03-22 22:55:41 +0800

    Create 题10.二叉树中和为某一值的所有路径.cpp

---
+++ target_offer/题10.二叉树中和为某一值的所有路径.cpp
@@ -0,0 +1,25 @@
+/*************************************************************************
+>> File Name: 二叉树中和为某一值的所有路径.cpp
+>> Author: chenjunjie
+>> Mail: 2716705056qq.com
+>> Created Time: 2019.03.22
+************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};
+
+class Solution {
+public:
+    vector<vector<int> > FindPath(TreeNode* root,int expectNumber) {
+
+    }
+};
