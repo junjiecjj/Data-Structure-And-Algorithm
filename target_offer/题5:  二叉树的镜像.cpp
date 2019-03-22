5354913fb0081a8053a36cb14d699d5b2e7088a0
junjiecjj <32957066+junjiecjj@users.noreply.github.com>
2019-03-22 15:02:07 +0800

    Create 题5:  二叉树的镜像.cpp

---
+++ target_offer/题5:  二叉树的镜像.cpp
@@ -0,0 +1,45 @@
+/*************************************************************************
+>> File Name: 二叉树的镜像.cpp
+>> Author: chenjunjie
+>> Mail: 2716705056qq.com
+>> Created Time: 2019.03.22
+
+
+题目描述
+操作给定的二叉树，将其变换为源二叉树的镜像。
+输入描述:
+
+二叉树的镜像定义：源二叉树 
+    	    8
+    	   /  \
+    	  6   10
+    	 / \  / \
+    	5  7 9 11
+
+    	镜像二叉树
+    	    8
+    	   /  \
+    	  10   6
+    	 / \  / \
+    	11 9 7  5
+
+************************************************************************/
+
+#include<iostream>
+using namespace std;
+#include<deque>
+
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};
+class Solution {
+public:
+    void Mirror(TreeNode *pRoot) {
+        
+    }
+};
