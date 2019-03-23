03da22f5bf1939f7afb0795ed4290b18417f6d55
junjiecjj <32957066+junjiecjj@users.noreply.github.com>
2019-03-23 13:59:35 +0800

    Create 题12.安之字形顺序打印二叉树.cpp

---
+++ target_offer/题12.安之字形顺序打印二叉树.cpp
@@ -0,0 +1,32 @@
+/*************************************************************************
+>> File Name: 按之字形顺序打印二叉树.cpp
+>> Author: chenjunjie
+>> Mail: 2716705056qq.com
+>> Created Time: 2019.03.23
+
+
+题目描述
+请实现一个函数按照之字形打印二叉树，即第一行按照从左到右的顺序打印，第二层按照从右至左的顺序打印，
+第三行按照从左到右的顺序打印，其他行以此类推。
+************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+    TreeNode(int x) :
+            val(x), left(NULL), right(NULL) {
+    }
+};
+
+
+class Solution {
+public:
+    vector<vector<int> > Print(TreeNode* pRoot) {
+        
+    }
+    
+};
