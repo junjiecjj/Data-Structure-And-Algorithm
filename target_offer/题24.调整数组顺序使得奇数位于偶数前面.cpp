a19b7e1f666742c5426868b749736fda062d39bb
junjiecjj <32957066+junjiecjj@users.noreply.github.com>
2019-03-31 19:54:11 +0800

    Update 题24.调整数组顺序使得奇数位于偶数前面.cpp

--- target_offer/题24.调整数组顺序使得奇数位于偶数前面.cpp
+++ target_offer/题24.调整数组顺序使得奇数位于偶数前面.cpp
@@ -15,7 +15,7 @@ using namespace std;
 
 class Solution {
 public:
-    void reOrderArray(vector<int> &vect)
+    void reOrderArray(vector<int> &vect)//稳定算法
     {
         vector<int> result;
 
@@ -32,5 +32,24 @@ public:
         }
         vect = result;
         
+    }
+    void reOrderArray1(vector<int> &vect)//不稳定算法
+    {
+        int len = vect.size();
+        int i = 0;
+        int j = len-1;
+        while(i < j)
+        {
+            while(i < j && vect[i]%2 == 1)
+                i++;
+            while(i < j && vect[j]%2 == 0)
+                j--;
+            if(i < j)
+            {
+                int tmp = vect[i];
+                vect[i] = vect[j];
+                vect[j] = tmp;
+            }
+        }
     }
 };
